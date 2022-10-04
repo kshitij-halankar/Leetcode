ae378ba2a3f6bb70f698f660437a8f00d173482c
kshitij-halankar <halanka@uwindsor.ca>
2022-10-04 12:37:49 +0000

    Sync LeetCode submission - Rearrange Products Table (oraclesql)

--- my-folder/problems/rearrange_products_table/solution.sql
+++ my-folder/problems/rearrange_products_table/solution.sql
@@ -1,4 +1,4 @@
-/* Write your T-SQL query statement below */
+/* Write your PL/SQL query statement below */
 select product_id, 'store1' as store, store1 as price from Products where store1 is not null
 union
 select product_id, 'store2' as store, store2 as price from Products where store2 is not null

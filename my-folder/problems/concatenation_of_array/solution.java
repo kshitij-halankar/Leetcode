f5fb9e0e5c2b12e29bbacb6cf73921fd84673a1f
kshitij-halankar <halanka@uwindsor.ca>
2023-10-18 13:04:40 +0000

    Sync LeetCode submission - Concatenation of Array (java)

--- my-folder/problems/concatenation_of_array/solution.java
+++ my-folder/problems/concatenation_of_array/solution.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int len = nums.length;
-        int len2 = len * 2;
-        int i = 0, j = len;
-        int[] a = new int[len2];
-        for (i = 0; i < len; i++) {
-            a[i] = nums[i];
-            a[j] = nums[i];
-            j++;
+        int i=0,n=nums.length;
+        int[] ans = new int[2*n];
+        for(i=0;i<n;i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
         }
-        return a;
+        return ans;
     }
 }

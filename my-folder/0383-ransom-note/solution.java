2b1049a5d7190e520555db1135ff8dfbe0590f1f
kshitij-halankar <halanka@uwindsor.ca>
2024-02-14 05:28:15 +0000

    [LeetCode Sync] Runtime - 5 ms (62.60%), Memory - 45 MB (50.84%)

--- my-folder/0383-ransom-note/solution.java
+++ my-folder/0383-ransom-note/solution.java
@@ -1,23 +1,51 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int count = 0;
-        char[] r = ransomNote.toCharArray();
-        char[] m = magazine.toCharArray();
-        for(int i = 0; i< r.length; i++) {
-            for(int j = 0; j < m.length; j++) {
-                if(r[i] == m[j]) {
-                    count++;
-                    StringBuilder sb = new StringBuilder();
-                    sb.append(m);
-                    sb.deleteCharAt(j);
-                    m = sb.toString().toCharArray();
-                    break;    
-                }
+        int i=0, j=0, r=ransomNote.length(), m=magazine.length();
+        if(r > m) return false;
+        char[] rn = ransomNote.toCharArray();
+        char[] mg = magazine.toCharArray();
+        Arrays.sort(rn);
+        Arrays.sort(mg);
+        // System.out.println(rn);
+        // System.out.println(mg);
+        while(i < r && j < m){
+            // System.out.println("rn: "+ rn[i] + " mg: " + mg[j]);
+            if (rn[i] == mg[j]) {
+                i++;
+                j++;
+            } else if (rn[i] < mg[j]) {
+                return false;
+            } else {
+                j++;
             }
         }
-        if (count == r.length)
-            return true;
-        else
+        if (j == m && i!= r) {
             return false;
+        }
+        return true;
     }
 }
+
+
+        // List<Character> rn = new ArrayList<>(
+        //     ransomNote.chars()
+        //     .mapToObj(c -> (char)c)
+        //     .collect(Collectors.toList())
+        // );
+        // List<Character> mg = new ArrayList<>(
+        //     magazine.chars()
+        //     .mapToObj(c -> (char)c)
+        //     .collect(Collectors.toList())
+        // );
+        // rn.sort();
+        // mg.sort();
+        // while(i < r){
+        //     if(rn.get(i) == mg.get(j)) {
+        //         i++;
+        //         j++;
+        //     } else if(rn.get(i) > mg.get(j)) {
+        //         return false;
+        //     } else {
+        //         j++;
+        //     }
+        // }
